fd33144a080a4ca048bbd80a0fa869942e434b0c
Ryan De Mesa <ryandemesa09@gmail.com>
2023-01-24 23:59:56 +0000

    Add logic for remaining exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ryan", last_name: "De Mesa", hourly_rate: 30)
+@store1.employees.create(first_name: "Natasha", last_name: "Sevilla", hourly_rate: 50)
+@store2.employees.create(first_name: "Milo", last_name: "Dogg", hourly_rate: 100)
+@store2.employees.create(first_name: "Bear", last_name: "Dogg", hourly_rate: 10)
+
+pp Employee.all
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,32 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, numericality: { only_integer: true }
+end
+
+class Store
+  validates :annual_revenue, :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+
+  def stores_must_carry_mens_or_womens_apparel
+    if mens_apparel = false && womens_apparel = false
+      errors.add(:expiration_date, "store must carry either men's or women's apparel")
+    end
+  end
+end
+
+def add_store 
+  
+  puts "Add a new store"
+  print "> "
+  user_input = $stdin.gets.chomp.to_s
+  new_store = Store.create(name: user_input)
+  pp new_store.errors.full_messages
+end
+
+add_store
